84aeff1d71ebb17412d87a8d3b711e27c8797023
심성진-PC\심성진 <kumoh130715@gmail.com>
2018-03-17 22:34:01 +0900

    몽구스를 이용하여 디비 정보 한번에 올리고 그 값이 잘들어 갔는지 확인하는 테스트(npm install mongoose 필요), 비룡이와 대화방식 틀 마련, db 변수에 mongodb서버 연결값 입력

--- index.js
+++ index.js
@@ -1,45 +1,104 @@
-var app = require('express')();
+﻿var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var Client = require('mongodb').MongoClient;
+var db;
 
-nicknames = []; // 닉네임 저장 배열 선언
+var userList = new Array();
+var typingList  = new Array(); // 타이핑 중인사람 배열 선언
+
+Client.connect('mongodb://localhost:27017/Biryong', function(error, database){
+    if(error) {
+        console.log(error);
+    } else {
+        // 1. 입력할 document 생성
+		db=database;
+    }
+});	// 데이터베이스 삽입 제대로 작동하는지 테스트
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-	socket.on("new user", function(data, callback) {
-      
-	  
-	  if (nicknames.indexOf(data) >= 0) {
+	var nickname;
+   function updateNicknames() {   // 접속 중인 멤버체크를 위한 메소드 구현 중
+      	io.sockets.emit('typing', { 
+		typingList : typingList 
+      ,userList : userList
+    });
+   }
+   
+  //핫솔이 new user 추가//
+  socket.on("new user", function(data, callback) {
+      if (userList.indexOf(data) != -1) {
          callback(false);
       } else {
          callback(true);
          socket.nickname = data;
-         nicknames[socket.nickname] = socket;
+		 nickname = data;
+         userList.push(nickname);
+         updateNicknames();
          console.log(data+' 입장!!');
-       socket.broadcast.emit("new user", data);
+		 socket.broadcast.emit("chat message", socket.nickname+'님이 입장하셨습니다.','비룡');
       }
-   }); // 유저 접속 알림,
-	
-	socket.on("disconnect", function(data) {
+   }); // 유저 접속 알림
+   
+   socket.on("disconnect", function(data) {
       if (!socket.nickname)
          return;
-      nicknames.splice(nicknames.indexOf(socket.nickname), 1);
-     console.log(socket.nickname+' 퇴장!!');
-     io.sockets.emit("disconnect", socket.nickname);
-     
+     var i = userList.indexOf(nickname);
+	 var j = typingList.indexOf(nickname);
+	console.log(userList[i]+' 퇴장!!');
+	userList.splice(i,1);
+	if(j != -1)
+		typingList.splice(j,1);
+	socket.broadcast.emit("chat message", socket.nickname+'님이 퇴장하셨습니다.','비룡');
+    updateNicknames();
    });
+   /////////////////////
+   
   socket.on('chat message', function(msg){
      //자기자신을 제외한 클라이언트 들에게 emit을 보내게 바꾸어줌
-    socket.broadcast.emit('chat message', msg, socket.nickname);	//닉네임 추가
+	 var i = msg.indexOf('비룡');
+	 var j = msg.indexOf('메뉴추천');
+	 var k = msg.indexOf('재료추천');
+	 if( i != -1)
+	 {
+		 if(j!=-1){	 
+			socket.broadcast.emit('chat message', msg, socket.nickname); 
+			io.sockets.emit('chat message', '메뉴추천/', '비룡');
+		 }
+		 else if(k!=-1){	 
+			socket.broadcast.emit('chat message', msg, socket.nickname); 
+			io.sockets.emit('chat message', '재료추천/', '비룡');
+		 }
+		 else{
+			socket.broadcast.emit('chat message', msg, socket.nickname); 
+			io.sockets.emit('chat message', '안녕하세요? 메뉴 추천 받으시려면 "메뉴추천" 가지고 계신 재료로 추천 받으시려면 "재료로 추천"을 입력해주세요!', '비룡');
+		 }			
+	 }
+	 else 
+		socket.broadcast.emit('chat message', msg, socket.nickname);
+	updateNicknames();
   });
+  
   //typing emit이 올시에 브로드캐스트방식으로 typing emit 클라이언트에게 보내기
-  socket.on('typing', function(msg){	
-    socket.broadcast.emit('typing', socket.nickname, msg);
-  });	// 닉네임 추가
+  socket.on('typing', function(msg){
+	if(msg == true) {
+		var i = typingList.indexOf(nickname);
+		if(i == -1){
+				typingList.push(nickname);
+		}
+	}
+	else{
+				var i = typingList.indexOf(nickname);
+				typingList.splice(i,1);
+	}
+	updateNicknames();
+  });
 });
 
 http.listen(port, function(){
