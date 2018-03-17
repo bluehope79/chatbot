2a108f6963bda023c4fa38bd07e3acc79ad94026
심성진-PC\심성진 <kumoh130715@gmail.com>
2018-03-17 22:37:45 +0900

    디비에서 비밀번호값 받아오게 하고, 관리자모드 추가를 위한 플래그 설정 및 틀 마련

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var db;
 var userList = new Array();
 var typingList  = new Array(); // 타이핑 중인사람 배열 선언
 
+
 Client.connect('mongodb://localhost:27017/Biryong', function(error, database){
     if(error) {
         console.log(error);
@@ -23,7 +24,26 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+	var dbpass;
+	var admin=0; //접근 했다고 알림 초기값 0
+    var master=0; // 패스워드 확인했다고 알림 초기값 0
 	var nickname;
+	
+	var query = {name:'pass'};
+        // 2. 읽어올 Field 선택
+	var projection = {passward:1};
+        // 3. find() 함수에 작성된 query와 projection을 입력
+	var cursor = db.collection('passward').find(query,projection);
+	cursor.each(function(err,doc){
+		if(err){
+			console.log(err);
+		}else{
+			if(doc != null){
+				dbpass=doc.passward;
+			}
+		}
+	}); // 비밀번호 db에서 받아오기
+	
    function updateNicknames() {   // 접속 중인 멤버체크를 위한 메소드 구현 중
       	io.sockets.emit('typing', { 
 		typingList : typingList 
@@ -65,7 +85,8 @@ io.on('connection', function(socket){
 	 var i = msg.indexOf('비룡');
 	 var j = msg.indexOf('메뉴추천');
 	 var k = msg.indexOf('재료추천');
-	 if( i != -1)
+	 var l = msg.indexOf('관리자모드');
+	 if( i != -1 )
 	 {
 		 if(j!=-1){	 
 			socket.broadcast.emit('chat message', msg, socket.nickname); 
@@ -75,11 +96,34 @@ io.on('connection', function(socket){
 			socket.broadcast.emit('chat message', msg, socket.nickname); 
 			io.sockets.emit('chat message', '재료추천/', '비룡');
 		 }
+		 else if(l!=-1){	 
+			socket.emit('chat message','관리자 비밀 번호를 입력하세요', '비룡');//자신에게만 보낼때
+			admin = 1;
+		 }
 		 else{
 			socket.broadcast.emit('chat message', msg, socket.nickname); 
-			io.sockets.emit('chat message', '안녕하세요? 메뉴 추천 받으시려면 "메뉴추천" 가지고 계신 재료로 추천 받으시려면 "재료로 추천"을 입력해주세요!', '비룡');
+			io.sockets.emit('chat message', '안녕하세요? 메뉴 추천 받으시려면 "메뉴추천" 가지고 계신 재료로 추천 받으시려면 "재료추천" 관리자이시면 "관리자모드"를 입력해주세요!', '비룡');
 		 }			
 	 }
+	 else if(admin == 1){
+		 
+		 
+		if(msg==dbpass){  
+			master = 1;
+            socket.emit('chat message','관리자님 안녕하세요. 관리자 모드는 메뉴 삽입 삭제 수정이 가능합니다.', '비룡');
+        }
+		else{
+			socket.emit('chat message','비밀번호 불일치!', '비룡');
+		}
+		admin=0;
+
+
+	}
+	else if(master == 1){//삽입,삭제,수정
+	    var input = msg.indexOf('삽입');
+        var del = msg.indexOf('삭제');
+		var revise = msg.indexOf('수정');
+	}
 	 else 
 		socket.broadcast.emit('chat message', msg, socket.nickname);
 	updateNicknames();
